4576ec7304a712dcf979ae665eae8827f7c2a730
sfumato00 <97776161+sfumato00@users.noreply.github.com>
2024-11-10 21:51:32 -0800

    init commit

---
+++ run_work_screen.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+for file in profile_*.tin; do
+    if [ -f "$file" ]; then
+        temp=${file#profile_}
+        session_name=${temp%.tin}
+
+        screen -X -S $session_name quit > /dev/null 2>&1
+        screen -dmS $session_name bash -c "tt++ $file; exec bash"
+
+        if screen -list | grep -q $session_name; then
+            echo "Screen session '$session_name' started successfully"
+        else
+            echo "Failed to start screen session"
+            exit 1
+        fi
+    fi
+done
+
