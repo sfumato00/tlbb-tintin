ab036eaa56a5ccf633418999971b0a8f2c6cc6a9
sfumato <sfumato000@gmail.com>
2025-01-14 18:59:42 +0000

    fix: 改进了自动降经验模块的鲁棒性

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ for file in profile_*.tin; do
         session_name=${temp%.tin}
 
         screen -X -S $session_name "quit"
-        screen -dmS "$session_name" bash -c "tt++ main.tin"
+        screen -dmS $session_name bash -c "tt++ main.tin; exec bash"
         screen -X -S "$session_name" -p 0 -X stuff "conn $session_name\n"
         echo "Screen session '$session_name' created."
     fi
