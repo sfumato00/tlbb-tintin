2a1726117b0a6207720ceb5ad000d15d33b910f8
sfumato <sfumato000@gmail.com>
2025-01-22 09:17:21 +0000

    update

--- stop.sh
+++ stop.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-for file in profile_*.tin; do
-    if [ -f "$file" ]; then
-        temp=${file#profile_}
-        session_name=${temp%.tin}
-
-        screen -X -S $session_name quit > /dev/null 2>&1
-    fi
-done
-
+screen -ls | awk '/\t/ {print $1}' | xargs -I {} screen -S {} -X quit
