ef890a0270698f7f4d176ddeb448115e8ca34b08
Sfumato <sfumato000@gmail.com>
2025-01-23 12:58:53 +0800

    chore: restructure project

--- run.sh
+++ run.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-for file in profile_*.tin; do
-    if [ -f "$file" ]; then
-        temp=${file#profile_}
-        session_name=${temp%.tin}
+# Exit immediately if a command exits with a non-zero status.
+set -e
 
-        screen -X -S $session_name "quit"
-        screen -dmS $session_name bash -c "tt++ main.tin; exec bash"
-        screen -X -S "$session_name" -p 0 -X stuff "conn $session_name\n"
-        echo "Screen session '$session_name' created."
-    fi
-done
+# Function to display usage instructions
+usage() {
+    echo "Usage: $0 <session_name>"
+    exit 1
+}
 
-sleep 3
+# Check if the session name is provided
+if [ -z "$1" ]; then
+    echo "Error: Session name is required."
+    usage
+fi
 
-for file in profile_*.tin; do
-    if [ -f "$file" ]; then
-        temp=${file#profile_}
-        session_name=${temp%.tin}
+# Extract session name from the first argument
+SESSION_NAME="$1"
 
-        screen -X -S "$session_name" -p 0 -X stuff "y\n"
-    fi
-done
+# Create the profile file path
+tin_profile="./profiles/profile_${SESSION_NAME}.tin"
+
+# Check if the profile file exists
+if [ ! -f "$tin_profile" ]; then
+    echo "Error: Profile file '$tin_profile' does not exist."
+    exit 1
+fi
+
+# Start a new screen session and run tintin++ with the profile file
+screen -dmS "$SESSION_NAME" bash -c "tt++ main.tin && exec bash"
+
+# Send 'conn <session_name>' and 'Enter' to the screen session
+screen -S "$SESSION_NAME" -p 0 -X stuff "conn ${SESSION_NAME}$(printf '\r')"
+
+# Switch into the screen session
+screen -r "$SESSION_NAME"
---
+++ run_all.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for file in profile_*.tin; do
+    if [ -f "$file" ]; then
+        temp=${file#profile_}
+        session_name=${temp%.tin}
+
+        screen -X -S $session_name "quit"
+        screen -dmS $session_name bash -c "tt++ main.tin; exec bash"
+        screen -X -S "$session_name" -p 0 -X stuff "conn $session_name\n"
+        echo "Screen session '$session_name' created."
+    fi
+done
+
+sleep 3
+
+for file in profile_*.tin; do
+    if [ -f "$file" ]; then
+        temp=${file#profile_}
+        session_name=${temp%.tin}
+
+        screen -X -S "$session_name" -p 0 -X stuff "y\n"
+    fi
+done
